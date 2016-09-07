eb2f6fe4aa9c1b4b80ded9bf79dc8fc2892cf906
Nathaniel Hatfield <nthatfield@gmail.com>
2016-09-07 12:25:42 -0400

    Create Function and Variable
    Created the helloWorld function to check user response, and the helloPrompt variable to prompt for said response.

--- Challenges/Week2/app.js
+++ Challenges/Week2/app.js
@@ -1,8 +1,15 @@
 //create function
+function helloWorld(message) {
+  if (message.toLowerCase() === "hello"){
+    alert("Hello World!");
+  } else {
+    alert("That's not what I was looking for.  Goodbye world!");
+  }
+}
   //control and return statements
 
 //Prompt user for input and store variable
-
+var helloPrompt = prompt("Hello?");
 //Call function
-
+helloWorld(helloPrompt);
 //Alert user results
